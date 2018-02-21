e770cb26444bff55de4e4c7f7b7ba72840167d16
Devin Matte <devinmatte@gmail.com>
2018-02-21 14:06:44 -0500

    Using Grunt for Icons

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,13 +37,76 @@ module.exports = function (grunt) {
                     'assets/js/util.min.js': 'assets/js/util.js'
                 }
             }
+        },
+        realFavicon: {
+            favicons: {
+                src: 'images/logo.svg',
+                dest: 'images/icons',
+                options: {
+                    iconsPath: 'images/icons',
+                    design: {
+                        ios: {
+                            pictureAspect: 'backgroundAndMargin',
+                            backgroundColor: '#ffffff',
+                            margin: '14%',
+                            assets: {
+                                ios6AndPriorIcons: false,
+                                ios7AndLaterIcons: false,
+                                precomposedIcons: false,
+                                declareOnlyDefaultIcon: true
+                            }
+                        },
+                        desktopBrowser: {},
+                        windows: {
+                            pictureAspect: 'noChange',
+                            backgroundColor: '#2b5797',
+                            onConflict: 'override',
+                            assets: {
+                                windows80Ie10Tile: false,
+                                windows10Ie11EdgeTiles: {
+                                    small: true,
+                                    medium: true,
+                                    big: true,
+                                    rectangle: true
+                                }
+                            }
+                        },
+                        androidChrome: {
+                            pictureAspect: 'noChange',
+                            themeColor: '#1F2028',
+                            manifest: {
+                                display: 'standalone',
+                                orientation: 'notSet',
+                                onConflict: 'override',
+                                declared: true
+                            },
+                            assets: {
+                                legacyIcon: true,
+                                lowResolutionIcons: false
+                            }
+                        },
+                        safariPinnedTab: {
+                            pictureAspect: 'silhouette',
+                            themeColor: '#1f2028'
+                        }
+                    },
+                    settings: {
+                        scalingAlgorithm: 'Mitchell',
+                        errorOnImageTooSmall: false,
+                        readmeFile: false,
+                        htmlCodeFile: false,
+                        usePathAsIs: false
+                    }
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-real-favicon');
 
-    grunt.registerTask('default', ['htmlmin', 'cssmin', 'uglify']);
+    grunt.registerTask('default', ['htmlmin', 'cssmin', 'uglify', 'realFavicon']);
 
 };
