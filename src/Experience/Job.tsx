ae52632ee94908db93eaa6adfa94c3aed5ffd581
Devin Matte <devinmatte@gmail.com>
2020-08-24 14:17:24 -0400

    Adding Apple

--- src/Experience/Job.tsx
+++ src/Experience/Job.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import '../styles/main.scss';
 
-type JobProps = {
-  range: string; 
+interface JobProps {
+  range: string;
   position: string;
   desciption: string;
 }
 
-const Job: React.FunctionComponent<JobProps> = ({position, range, desciption}) => {
+const Job: React.FunctionComponent<JobProps> = ({ position, range, desciption }) => {
   return (
     <div>
       <h5>{position} | {range}</h5>
-			<p>{desciption}</p>
-		</div>
+      <p>{desciption}</p>
+    </div>
   );
 }
 
--- src/Project/Project.tsx
+++ src/Project/Project.tsx
@@ -7,26 +7,25 @@ type ProjectProps = {
   imageKey: string;
 }
 
-const Project: React.FunctionComponent<ProjectProps> = (props) => {
-  const {name, description, imageKey, children} = props;
+const Project: React.FunctionComponent<ProjectProps> = ({ name, description, imageKey, children }) => {
   const imageWebpSource = "images/projects/webp/" + encodeURI(imageKey) + ".webp"
   const imagePngSource = "images/projects/png/" + encodeURI(imageKey) + ".png"
   return (
     <article>
-        <div className="image">
-            <picture className="image">
-                <source srcSet={imageWebpSource} type="image/webp"/>
-                <source srcSet={imagePngSource} type="image/png"/>
-                <img src={imagePngSource} data-position="center" alt={name + " Screenshot"}/>
-            </picture>
-        </div>
-		<div className="content">
-			<h3>{name}</h3>
-			<p>{description}</p>
-			<ul className="actions">
-				{children}
-			</ul>
-		</div>
+      <div className={"image"}>
+        <picture className={"image"}>
+          <source srcSet={imageWebpSource} type={"image/webp"} />
+          <source srcSet={imagePngSource} type={"image/png"} />
+          <img src={imagePngSource} data-position={"center"} alt={name + " Screenshot"} />
+        </picture>
+      </div>
+      <div className={"content"}>
+        <h3>{name}</h3>
+        <p>{description}</p>
+        <ul className={"actions"}>
+          {children}
+        </ul>
+      </div>
     </article>
   );
 }
